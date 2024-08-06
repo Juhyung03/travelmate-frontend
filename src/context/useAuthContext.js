ee7e489e9e120bfac1847be5a7c0bceccaa87dd4
WhileHarold <whileharold@gmail.com>
2024-08-06 17:11:30 +0900

    refactor: context 폴더 내 코드 리팩토링

--- src/context/useAuthContext.js
+++ src/context/useAuthContext.js
@@ -1,11 +1,9 @@
-import { create } from 'zustand';
+import create from 'zustand';
 
 const useAuthStore = create((set) => ({
-  isAuthenticated: false,
-  user: null, // 사용자 정보를 저장할 상태 변수 추가
-  setIsAuthenticated: (authStatus) => set({ isAuthenticated: authStatus }),
-  setUser: (user) => set({ user }), // 사용자 정보를 설정하는 함수
-  clearAuth: () => set({ isAuthenticated: false, user: null }), // 인증 상태와 사용자 정보 초기화
+  user: null,
+  setUser: (user) => set({ user }),
+  clearUser: () => set({ user: null }),
 }));
 
 export default useAuthStore;
